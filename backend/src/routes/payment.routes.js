a70fb17e8c9406fe6da911cdc4565ab68b5793cd
Jean Bonilla <snapjean790@gmail.com>
2025-11-09 07:59:14 -0600

    Happy Coding

--- backend/src/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { simulatePayment } from '../controllers/payment.controller.js';
-
-const router = express.Router();
-
-router.post('/simulate', simulatePayment);
-
-export default router;
